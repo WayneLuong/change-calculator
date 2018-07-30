71338303701ed5cc5c29b9fbbf2c347e6f7f1557
Wayne Luong <wayneluong93@gmail.com>
2018-07-30 14:49:27 +0100

    initial upload of complete application

---
+++ ChangeCalculator/ChangeCalculator/ChangeCalculator/ChangeCalculator/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ChangeCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Declarations
+            double cost = 0;
+            double paid = 0;
+
+            Console.WriteLine("\t===============Wayne's Change Calculator===============\n");
+            //Reading Input
+
+            //Type Validation check for input
+            Console.Write("\t\tPlease Enter Cost of Item: ");
+            try
+            {
+                cost = Double.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Please Enter a Number!", "Error!");
+                Console.Clear();
+                Main();
+            }
+
+            //Type Validation check for input
+            Console.Write("\t\tPlease Enter Amount Paid: ");
+            try
+            {
+                paid = Double.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Please Enter a Number!", "Error!");
+                Console.Clear();
+                Main();
+            }
+
+            Console.WriteLine("\n\t\t\tItem cost: £" + cost);
+            Console.WriteLine("\t\t\tAmount paid: £" + paid + "\n");
+            
+            //Calling calculateChange.amountOfCoins methods
+            //Checks if paid amount is more than cost
+            if (cost <= paid) 
+            {
+                double change = Math.Round(paid - cost, 4);
+
+               Console.WriteLine("\t\t\tChange Due: £" + change + "\n");
+
+                var ukSterling = new ukSterling();
+                ukSterling.amountOfCoins(change);
+
+            } else 
+            {
+                MessageBox.Show("You entered less than the cost! Please Retry.","Warning!");
+                Console.Clear();
+                Main();
+            }
+
+           
+            //Exit/Restart
+            Console.Write("\n\t\tPress any Key to Restart or E to exit");
+            ConsoleKeyInfo Keypress;
+            Keypress = Console.ReadKey();
+            if (Keypress.Key == ConsoleKey.E)
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.Clear();
+                Main();
+            }
+            
+        }
+    }
+}
---
+++ ChangeCalculator/ChangeCalculator/ChangeCalculator/ChangeCalculator/calculateChange.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChangeCalculator
+{
+
+    class ukSterling
+    {
+        //Declarations
+        double[] coins = { 0.75, 0.15, 0.03, 0.01, 0.005, 0.0025 };
+
+        //Methods
+        public void amountOfCoins(double input)
+        {
+          int numOfCoins = 0;
+          var index = 0;
+          double remainder = 0;
+
+          Console.WriteLine("\t\t  Your change in (uk sterling):\n");
+          //Iterate through the coins array
+          for (var i=0; i<coins.Length; i++)
+           {
+               remainder = input % coins[i];
+                while (input > coins[i])
+                {
+                    input = input - coins[i];
+                    numOfCoins++;
+                    index = i;
+                }
+                
+                //If rounded number equals last array value increment the numOfCoins
+                if (Math.Round(remainder, 4) == 0.0025)
+                {
+                    numOfCoins++;
+                } 
+                
+                //Checks if numOfCoins has a value before printing
+                if (numOfCoins != 0)
+                {
+                    Console.WriteLine("\t\t\t" + numOfCoins + "   x   " + coins[i] + "p   =   " + numOfCoins * coins[i] + "p");
+                }
+                                
+                //Resets the counter for next coin
+                numOfCoins = 0;
+            }
+
+          //Prints any remainder that the smallest coin value couldn't fill
+          remainder = input % coins[index];
+          if (Math.Round(remainder, 5) != coins[index] && remainder != 0)
+            {
+             Console.WriteLine("\n\t\tNote: Remainder of " + Math.Round(remainder, 5) + "p!(Rounded Value)");
+            }
+        }
+    }
+}
